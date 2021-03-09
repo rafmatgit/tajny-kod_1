8f25e4753ceb80491e8d7cac9ea78f38678301fb
DonRafal <rafalmatusik1@gmail.com>
2021-03-09 17:49:52 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import './style.css';
+import "./style.css";
 
-document.querySelector('.display').textContent = '1234';
+const display = (document.querySelector(".display").textContent = "1234");
+const resetBtn = document.querySelector(".keybord_button--reset");
+const saveBtn = document.querySelector(".keyboard_button--save");
+const keyboard = document.querySelectorAll(".keyboard_button");
+const displayArr = [];
